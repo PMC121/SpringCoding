301930874b61921be483b1ebac35f86d46bb9ea3
PMC121 <prashantchavan712@gmail.com>
2021-02-13 12:35:43 +0530

    Fisrt Commits

--- src/main/java/com/junit/demo/SpringJunitApplication.java
+++ src/main/java/com/junit/demo/SpringJunitApplication.java
@@ -2,13 +2,19 @@ package com.junit.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SpringJunitApplication {
+public class SpringJunitApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJunitApplication.class, args);
 	}
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(SpringJunitApplication.class);
+	  }
 
 }
